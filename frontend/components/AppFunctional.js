5cf0f700a7e76958fe3dd9fbf4e90326c0bc90ee
catfishfishcat10101 <catfishfishcat10101@gmail.com>
2023-09-14 04:49:39 -0500

    finished AppFunctional.js

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 // Suggested initial states
 const initialMessage = ''
@@ -6,41 +7,87 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
+
 export default function AppFunctional(props) {
+  const [message, setMessage] = useState(initialMessage);
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
   function getXY() {
+    const x = (initialIndex % 3) + 1;
+    const y = Math.floor(initialIndex / 3) + 1;
+    return {x, y};
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
   }
 
   function getXYMessage() {
+    const { x, y } = getXY();
+    return `Coordinates (${x}, ${y})`;
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
   }
 
   function reset() {
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
     // Use this helper to reset all states to their initial values.
   }
 
   function getNextIndex(direction) {
+    let nextIndex = initialIndex;
+    switch (direction) {
+      case 'left':
+        if (nextIndex % 3 !== 0) {
+          nextIndex -= 1;
+        }
+        break;
+        case 'up':
+          if (nextIndex >= 3) {
+            nextIndex -= 3;
+          }
+          break;
+          case 'right':
+            if (nextIndex % 3 !== 2) {
+              nextIndex += 1;
+            }
+            break;
+            default:
+            break;
+    }
+    return nextIndex;
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
   }
 
-  function move(evt) {
+  function move(direction) {
+    const nextIndex = getNextIndex(direction);
+    if (nextIndex !== initialIndex) {
+      setIndex(nextIndex);
+      setSteps(prevSteps => prevSteps + 1);
+    }
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
 
   function onChange(evt) {
+    setEmail(evt.target.value);
     // You will need this to update the value of the input.
   }
 
-  function onSubmit(evt) {
+  function onSubmit(evt) { 
+    evt.preventDefault();
+    axios.post('/api/endpoint', { email })
+    .then(response => {
+      setMessage(response.data.message);
+    })
+    .catch(error => {
+      setMessage('An error occurred');
+    });
     // Use a POST request to send a payload to the server.
   }
 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import server from './backend/server';
+
 const server = require('./backend/server')
 
 const PORT = process.env.PORT || 9000
 
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
-})
+});
